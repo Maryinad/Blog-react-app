49cefa614d34184a1921c1a2cebbca6e6d495a62
Adam Doniak <adamdoniak@gmail.com>
2023-11-15 20:38:33 +0100

    edit react router

---
+++ src/components/BlogPreviewCard.jsx
@@ -0,0 +1,10 @@
+export default function BlogPreviewCard(){
+    return (
+        <article>
+            <span>Date</span>
+            <h2>Lorem ipsum dolor sit amet.</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut, commodi molestiae tempora explicabo inventore rerum eius. Doloremque dolores nihil nam?</p>
+            <img src="https://picsum.photos/200/300" />
+        </article>
+    )
+}
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import BlogPreviewCard from "../components/BlogPreviewCard";
+
+export default function HomePage(){
+    return <main>
+        <BlogPreviewCard />
+        <BlogPreviewCard />
+        <BlogPreviewCard />
+
+        </main>
+}
